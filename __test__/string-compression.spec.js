5bd1c8a34d8763e69e68a0933d1e20605b3ed57b
bsoetarto7 <bsoetarto7@gmail.com>
2017-11-15 14:20:55 -0800

    Fix bugs with the algorithm

--- __test__/string-compression.spec.js
+++ __test__/string-compression.spec.js
@@ -18,10 +18,28 @@ describe('compression', () => {
       expect(() => compression(345)).toThrow();
     });
   });
-  describe('when aaabbbcccd is input string', () => {
-    it('should return value a3b3c3d1', () => {
-      const results = compression('aaabbbcccd')
-      expect(results).toEqual('a3b3c3d1');
+  describe('when aaabbbcccddff is input string', () => {
+    it('should return value a3b3c3d2f2', () => {
+      const results = compression('aaabbbcccddff')
+      expect(results).toEqual('a3b3c3d2f2');
+    });
+  });
+  describe('when aaabbbccc is input string', () => {
+    it('should return value a3b3c3', () => {
+      const results = compression('aaabbbccc')
+      expect(results).toEqual('a3b3c3');
+    });
+  });
+  describe('when ghjff is input string', () => {
+    it('should return value g1h1j1f2', () => {
+      const results = compression('ghjff')
+      expect(results).toEqual('g1h1j1f2');
+    });
+  });
+  describe('when ghjffg is input string', () => {
+    it('should return value g1h1j1f2g1', () => {
+      const results = compression('ghjffg')
+      expect(results).toEqual('g1h1j1f2g1');
     });
   });
 })
